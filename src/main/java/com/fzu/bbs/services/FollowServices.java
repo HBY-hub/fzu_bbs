2673d4f4ebc4b79f9405074740144989824c3693
Qiuxinhan <120975969@qq.com>
2021-09-24 15:33:55 +0800

    add methods to FollowServices: boolean addFollow(User fromUser,User toUser); boolean deleteFollow(User fromUser,User toUser);

--- src/main/java/com/fzu/bbs/services/FollowServices.java
+++ src/main/java/com/fzu/bbs/services/FollowServices.java
@@ -1,4 +1,10 @@
 package com.fzu.bbs.services;
 
+import com.fzu.bbs.po.User;
+
+import java.util.List;
+
 public interface FollowServices {
+    boolean addFollow(User fromUser,User toUser);
+    boolean deleteFollow(User fromUser,User toUser);
 }
