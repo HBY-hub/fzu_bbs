451805046887558733aa499ab8aa78e3717d2277
Qiuxinhan <120975969@qq.com>
2021-09-24 15:30:14 +0800

    add methods: boolean addUser(User user); boolean updateUserPqssword(int id,String password); User getUserById(int id); List<User> getUserByName(String username);

--- src/main/java/com/fzu/bbs/services/UserServices.java
+++ src/main/java/com/fzu/bbs/services/UserServices.java
@@ -1,4 +1,12 @@
 package com.fzu.bbs.services;
 
+import com.fzu.bbs.po.User;
+
+import java.util.List;
+
 public interface UserServices {
+    boolean addUser(User user);
+    boolean updateUserPqssword(int id,String password);
+    User getUserById(int id);
+    List<User> getUserByName(String username);
 }
